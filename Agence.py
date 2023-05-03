91bec60c7c63613f4244ceab0bfe7eeff7cbe29b
Dogit <132139293+D0git@users.noreply.github.com>
2023-05-03 20:45:37 +0200

    Add files via upload

---
+++ Agence.py
@@ -0,0 +1,68 @@
+
+class Agence:
+
+    # Constructeur de classe agence :
+    
+    def __init__(self,c=None,v=None,t=None,a=None) :
+        self.code=c
+        self.ville=v
+        self.tel=t
+        self.adresse=a
+
+
+    # Creation d'une liste vide des agences :
+
+    def __init__(self):
+        self.Agence=[]
+
+
+
+
+
+    # Affichage des informations :
+
+    def Affichage(self):
+        print("Code :" ,self.code,
+               "\nVille :" ,self.ville,
+               "\nTel :" ,self.tel,
+               "\nAdresse :" ,self.adresse)
+
+    # Getters :-----------------------------------------------------------------------------------------
+
+    # Code :
+    def get_Code(self):
+        return self.code
+    
+    # Ville :
+    def get_Ville(self):
+        return self.ville
+    
+    # Tel :
+    def get_Tel(self):
+        return self.tel
+    
+    # Adresse :
+    def get_Adresse(self):
+        return self.adresse
+    
+
+    # Setters: ---------------------------------------------------------------------------------------
+
+    # Code :
+    def set_Code(self,c):
+        self.code=c
+    
+    # Ville :
+    def set_Ville(self,v):
+        self.ville=v
+    
+    # Tel :
+    def set_Tel(self,t):
+        self.tel=t
+    
+    # Adresse :
+    def set_Adresse(self,a):
+        self.adresse=a
+
+    
+
