3428dd014e504598b046a293c001d64aeadf03a2
Dogit <132139293+D0git@users.noreply.github.com>
2023-05-07 03:02:02 +0200

    Add files via upload

--- Voiture.py
+++ Voiture.py
@@ -1,9 +1,9 @@
-from Agence import *
+
     # Importer le module SQLite :
 import sqlite3
 
     # Se connecter à la base données :
-cnx=sqlite3.connect('Location_voiture.db')
+cnx=sqlite3.connect('Location_voiture1.db')
 
     # Fermeture de la connexion à la base de données :
 #cnx.close()
@@ -44,45 +44,61 @@ class Voiture():
 # Fonctions de recherche :
 
     # Recherche par marque :
-    def Rechercher_par_marque(self):
-        cursor.execute("select * from Voiture group by marque")
+    def Rechercher_par_marque(self,marque):
+        sql="select * from Voiture where marque=?"
+        val=(marque,)
+        cursor.execute(sql,val)
         result=cursor.fetchall()
         return result
 
     # Recherche par type de carburant :
-    def Rechercher_par_tcarb(self):
-        cursor.execute("select * from Voiture group by type_car")
+    def Rechercher_par_tcarb(self,type_car):
+        sql="select * from Voiture where type_car=?"
+        val=(type_car,)
+        cursor.execute(sql,val)
         result=cursor.fetchall()
         return result
     
     # Rechercher par transmission :
-    def Rechercher_par_trans(self):
-        cursor.execute("select * from Voiture group by transmission")
+    def Rechercher_par_trans(self,transmission):
+        sql="select * from Voiture where transmission=?"
+        val=(transmission,)
+        cursor.execute(sql,val)
         result=cursor.fetchall()
         return result  
       
     # Recherche par nombre de places :
-    def Rechercher_par_nbr_plc(self):
-        cursor.execute("select * from Voiture group by nbr_plc")
+    def Rechercher_par_nbr_plc(self,nbr_plc):
+        sql="select * from Voiture where nbr_plc=?"
+        val=(nbr_plc,)
+        cursor.execute(sql,val)
         result=cursor.fetchall()
         return result     
-
-    
         
     # Rechercher par prix de location par jours :
-    def Rechercher_par_prix_loc_jrs(self):
-        cursor.execute("select * from Voiture group by prix_loc_j")
+    def Rechercher_par_prix_loc_jrs(self,prix_loc_j):
+        sql="select * from Voiture where prix_loc_j=?"
+        val=(prix_loc_j,)
+        cursor.execute(sql,val)
         result=cursor.fetchall()
         return result
-    
+
+
 
     #Fonction d'ajout d'une voiture :
-    def Ajouter(self,matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car):    
-        sql="insert into Voiture(matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car,disponibilite) values(?,?,?,?,?,?,?,?,FALSE)"
-        val=(matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car) 
-        cursor.execute(sql,val)
-        cnx.commit()  
+    def Ajouter(self,matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car,code_ag):    
+        cursor.execute("select * from Agence where code_ag=?", (code_ag,))
+        ag=cursor.fetchone()
+        if ag==None:
+            return False
+        else:
+            sql="insert into Voiture (matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car,code_ag,disponibilite) values(?,?,?,?,?,?,?,?,?,FALSE)"
+            val=(matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car,code_ag)
+            cursor.execute(sql,val)
+        cnx.commit()
         return True
+            
+
          
     # Fonction de modification de l'image d'une voiture :
     def Modifier_image(self,image,matricule):
@@ -136,32 +152,39 @@ print("")
 y=v.Afficher_v_dispo()
 print(y)
 print("")
+
 """
 print("---Recherche_par_marque-----------------------------------------------------------------------------------------")
 print("")
-mrq=v.Rechercher_par_marque()
+marque=input("Maque :")
+mrq=v.Rechercher_par_marque(marque)
 print(mrq)
 print("")
 print("---Rechercher_par_tcarb-----------------------------------------------------------------------------------------")
 print("")
-tc=v.Rechercher_par_tcarb()
+type_car=input("Type carburant :")
+tc=v.Rechercher_par_tcarb(type_car)
 print(tc)
 print("")
 print("---Rechercher_par_trans-----------------------------------------------------------------------------------------")
 print("")
-tr=v.Rechercher_par_trans()
+transmission=input("Transmission :")
+tr=v.Rechercher_par_trans(transmission)
 print(tr)
 print("")
 print("---Rechercher_par_nbr_plc-----------------------------------------------------------------------------------------")
 print("")
-nbr=v.Rechercher_par_nbr_plc()
+nbr_plc=int(input("Nombre de places :"))
+nbr=v.Rechercher_par_nbr_plc(nbr_plc)
 print(nbr)
 print("")
 print("---Rechercher_par_prix_loc_jrs-----------------------------------------------------------------------------------------")
 print("")
-prix=v.Rechercher_par_prix_loc_jrs()
-print(nbr)
+prix_loc_j=float(input("Prix de location par jour :"))
+prix=v.Rechercher_par_prix_loc_jrs(prix_loc_j)
+print(prix)
 
+"""
 
 print("")
 print("---Ajouter voiture-----------------------------------------------")
@@ -177,8 +200,9 @@ modele=input("modele: ")
 marque=input("marque: ")
 type_car=input("type_car: ")
 disponibilite=False
+code_ag=int(input("Code agence :"))
 
-ajout=v.Ajouter(matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car)
+ajout=v.Ajouter(matricule,image,nbr_plc,prix_loc_j,transmission,modele,marque,type_car,code_ag)
 print(ajout)
 
 print("")
@@ -187,7 +211,7 @@ print("")
 y=v.Afficher_v_dispo()
 print(y)
 
-print("")
+"""print("")
 print("---Modifier voiture---------------------------------------------------------------------------------------------")
 print("---MODIFICATION IMAGE :-----------")
 print("")
